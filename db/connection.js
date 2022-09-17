b6f6831b3e1f68e4c8fd5e634bab35cd10c303ae
Cocotero98 <aguilarag98@gmail.com>
2022-09-17 00:10:43 -0300

    .

---
+++ db/connection.js
@@ -0,0 +1,57 @@
+const {MongoClient} = require('mongodb');
+const dotenv = require("dotenv");
+dotenv.config();
+let db;
+
+async function dbConnect() {
+    //Cluster0 connection string
+	// const uri = "mongodb+srv://agustin98:<password>@cluster0.1vqmd5v.mongodb.net/?retryWrites=true&w=majority;"
+    const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.1vqmd5v.mongodb.net/?retryWrites=true&w=majority`;
+
+    //Create new client with connection string
+    const client = new MongoClient(uri);
+
+    //Wrap our calls to functions that interact with the database in a try/catch statement so that we handle any unexpected errors.
+    try{
+       //Connect to our cluster. Next line returns a promise. We use await to stop further excecution until this is completed
+        await client.connect()
+        .then(db=client);
+
+        await listDocuments(client); 
+    } catch (e) {
+        console.error(e);
+    }
+    
+    //Close connection
+    finally {
+        await client.close();
+    }
+
+}
+
+const getDb = () => {
+    if (!db) {
+      throw Error('Db not initialized');
+    }
+    return db;
+  };
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+ 
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+
+async function listDocuments(client){
+    databaseCSE341 = await client.db().collection('contacts');
+
+    console.log("Documents:");
+    console.log(databaseCSE341);    
+}
+
+// main().catch(console.error);
+module.exports = { 
+                    dbConnect,
+                    getDb,
+                 };
--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//Trying to connect
+const connection = require('./db/connection')
+
 app.use('/', require('./routes'))
+// app.use('/contacts/', require('./routes/contacts'))
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+connection.dbConnect().catch(console.error);
